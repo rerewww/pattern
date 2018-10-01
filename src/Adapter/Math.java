9e056f590fff1baa3bfca52e0f31fa8c2fc68bae
rerewww <rerewww@naver.com>
2018-10-02 00:31:48 +0900

    master-study/strategy - 어댑터 패턴에 대한 코드     - 연관성 없는 두 객체 묶어서 사용하기

---
+++ src/Adapter/Math.java
@@ -0,0 +1,14 @@
+package Adapter;
+
+/**
+ * Created by son on 2018-10-02.
+ */
+public class Math {
+    public double twoTime(final double num) {
+        return num * 2;
+    }
+
+    public double half(final double num) {
+        return num / 2;
+    }
+}
